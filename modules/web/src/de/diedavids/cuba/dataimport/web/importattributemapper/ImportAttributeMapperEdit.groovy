c8628360718de4bec9532ae5f5cb0115c1427bd1
Mario <mario@die-davids.de>
2018-04-06 12:45:54 +0200

    (#8) ability to exclude columns in wizard

---
+++ modules/web/src/de/diedavids/cuba/dataimport/web/importattributemapper/ImportAttributeMapperEdit.groovy
@@ -0,0 +1,7 @@
+package de.diedavids.cuba.dataimport.web.importattributemapper
+
+import com.haulmont.cuba.gui.components.AbstractEditor
+import de.diedavids.cuba.dataimport.entity.ImportAttributeMapper
+
+class ImportAttributeMapperEdit extends AbstractEditor<ImportAttributeMapper> {
+}
