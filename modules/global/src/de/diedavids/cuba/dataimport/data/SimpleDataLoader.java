71f8a92632b018abd3cb1a4bbac6053b91651311
Mario David <mario@die-davids.de>
2018-04-20 08:49:01 +0200

    switched to Entity in SimpleDataLoader (#37)

--- modules/global/src/de/diedavids/cuba/dataimport/data/SimpleDataLoader.java
+++ modules/global/src/de/diedavids/cuba/dataimport/data/SimpleDataLoader.java
@@ -1,6 +1,5 @@
 package de.diedavids.cuba.dataimport.data;
 
-import com.haulmont.cuba.core.entity.BaseUuidEntity;
 import com.haulmont.cuba.core.entity.Entity;
 
 import java.util.Collection;
@@ -11,23 +10,23 @@ public interface SimpleDataLoader {
 
     String NAME = "ddcdi_SimpleDataLoader";
 
-    <E extends BaseUuidEntity> E load(Class<E> entityClass, UUID id);
-    <E extends BaseUuidEntity> E load(Class<E> entityClass, UUID id, String view);
+    <E extends Entity> E load(Class<E> entityClass, UUID id);
+    <E extends Entity> E load(Class<E> entityClass, UUID id, String view);
 
-    <E extends BaseUuidEntity> E loadByProperty(Class<E> entityClass, String propertyPath, Object propertyValue);
-    <E extends BaseUuidEntity> E loadByProperty(Class<E> entityClass, String propertyPath, Object propertyValue, String view);
+    <E extends Entity> E loadByProperty(Class<E> entityClass, String propertyPath, Object propertyValue);
+    <E extends Entity> E loadByProperty(Class<E> entityClass, String propertyPath, Object propertyValue, String view);
 
-    <E extends BaseUuidEntity> E loadByReference(Class<E> entityClass, String propertyPath, BaseUuidEntity reference);
-    <E extends BaseUuidEntity> E loadByReference(Class<E> entityClass, String propertyPath, BaseUuidEntity reference, String view);
+    <E extends Entity> E loadByReference(Class<E> entityClass, String propertyPath, Entity reference);
+    <E extends Entity> E loadByReference(Class<E> entityClass, String propertyPath, Entity reference, String view);
 
-    <E extends BaseUuidEntity> Collection<E> loadAll(Class<E> entityClass);
-    <E extends BaseUuidEntity> Collection<E> loadAll(Class<E> entityClass, String view);
+    <E extends Entity> Collection<E> loadAll(Class<E> entityClass);
+    <E extends Entity> Collection<E> loadAll(Class<E> entityClass, String view);
 
 
     <E extends Entity> Collection<E> loadAllByAttributes(Class<E> entityClass, Collection<EntityAttributeValue> entityAttributeValues);
 
-    <E extends BaseUuidEntity> Collection<E> loadAllByProperty(Class<E> entityClass, String propertyPath, Object propertyValue);
-    <E extends BaseUuidEntity> Collection<E> loadAllByProperty(Class<E> entityClass, String propertyPath, Object propertyValue, String view);
+    <E extends Entity> Collection<E> loadAllByProperty(Class<E> entityClass, String propertyPath, Object propertyValue);
+    <E extends Entity> Collection<E> loadAllByProperty(Class<E> entityClass, String propertyPath, Object propertyValue, String view);
 
 
 }
