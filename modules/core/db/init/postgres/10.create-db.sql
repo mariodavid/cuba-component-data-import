db708f4024cb430cde9f68a2e88f5cece147300c
Mario <mario@die-davids.de>
2018-07-21 23:40:07 +0200

    fixed missing attributes of postgres for latest updates

--- modules/core/db/init/postgres/10.create-db.sql
+++ modules/core/db/init/postgres/10.create-db.sql
@@ -81,7 +81,10 @@ create table DDCDI_IMPORT_ATTRIBUTE_MAPPER (
     DELETED_BY varchar(50),
     --
     CONFIGURATION_ID uuid not null,
+    MAPPER_MODE varchar(50) not null,
+    ATTRIBUTE_TYPE varchar(50),
     ENTITY_ATTRIBUTE varchar(255),
+    ASSOCIATION_LOOKUP_ATTRIBUTE varchar(255),
     FILE_COLUMN_NUMBER integer not null,
     FILE_COLUMN_ALIAS varchar(255),
     CUSTOM_ATTRIBUTE_BIND_SCRIPT text,
